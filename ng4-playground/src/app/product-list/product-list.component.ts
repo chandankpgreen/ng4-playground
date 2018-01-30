aaa3ce2d8f15d37b34196ddda20c77c99da3d569
Chandan Kumar <chandankpgreen@gmail.com>
2018-01-30 22:50:56 +0530

    commit 4

--- ng4-playground/src/app/product-list/product-list.component.ts
+++ ng4-playground/src/app/product-list/product-list.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-product-list',
+  selector: 'pm-products',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
 
+  pageTitle: string = 'Product List';
   constructor() { }
 
   ngOnInit() {
