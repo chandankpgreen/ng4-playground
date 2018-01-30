7278140b58db8c0d5177259c1960e736221b543d
chandank <chandank@meditab.com>
2018-01-30 19:06:59 +0530

    2nd commit

--- ng4-playground/src/app/app.component.ts
+++ ng4-playground/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'pm-root',
+  template: `
+  <div>
+    <h1>{{pageTitle}}</h1>
+    <div>My first component</div>
+  </div>
+  `
 })
 export class AppComponent {
-  title = 'app';
+  pageTitle: string = "Acme Product Management";
 }
